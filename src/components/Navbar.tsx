311c556c5c047646e62630cc8b2215c431090258
Ciprian Vlad <supercoder.cipi@gmail.com>
2024-07-04 11:48:26 +0200

    setup first commit

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,24 @@
+const Navbar = () => {
+    return (
+        <nav>
+            <div>
+                <a href="#">
+                    <h1>cipi vlad</h1>
+                </a>
+            </div>
+
+            <div>
+                {/* github icon */}
+
+                {/* linkedin icon */}
+
+                {/* email icon */}
+
+                {/* dev.to icon */}
+
+                {/* frontend mentor icon */}
+            </div>
+        </nav>
+    )
+}
+export default Navbar
---
+++ src/page/LandingPage.tsx
@@ -0,0 +1,10 @@
+import Navbar from "../components/Navbar"
+
+const LandingPage = () => {
+    return (
+        <>
+            <Navbar />
+        </>
+    )
+}
+export default LandingPage
