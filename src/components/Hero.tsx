89088c30901dd31255505efa1dd300c52fac3d6e
Ciprian Vlad <supercoder.cipi@gmail.com>
2024-07-05 10:40:02 +0200

    hero section add changes

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -7,13 +7,13 @@ const Hero = () => {
                 <div className="hero-text">
                     <h1>Nice to meet you!</h1>
                     <h2></h2>
-                    <p>I'm Web Developer.</p>
-                    <p>I'm passionate about creating beautiful and accessible websites. </p>
+                    <p>I'm a Web Developer.</p>
+                    <p>Passionate about creating beautiful and accessible websites. </p>
                     <p>I'm free for interesting projects.</p>
                     <a href="#contact">Get in touch</a>
                 </div>
-                <div className="hero-image">
-                    <div className="hero-image-container">
+                <div className="hero-image-container">
+                    <div className="hero-image">
                     </div>
                 </div>
             </div>
