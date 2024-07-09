ffcd4b4a1e75857d0ceca5854d88467ee69a4982
Ciprian Vlad <supercoder.cipi@gmail.com>
2024-07-09 11:15:57 +0200

    added svg contact

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -7,7 +7,27 @@ const Contact = () => {
             </div >
             <div className="contact-mail">
                 <a href="mailto:supercoder.cipi@gmail.com">write me!</a>
+
             </div>
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="530"
+                height="129"
+            // style={{
+            //     position: "absolute",
+            //     bottom: "0",
+            //     left: "50%",
+            //     transform: "translateX(-50%)"
+            // }}
+            >
+                <g fill="none" fill-rule="evenodd" stroke="#FFF" opacity=".25">
+                    <ellipse cx="265" cy="40" rx="264.5" ry="39.5" />
+                    <ellipse cx="265" cy="52" rx="264.5" ry="39.5" />
+                    <ellipse cx="265" cy="65" rx="264.5" ry="39.5" />
+                    <ellipse cx="265" cy="77" rx="264.5" ry="39.5" />
+                    <ellipse cx="265" cy="89" rx="264.5" ry="39.5" />
+                </g>
+            </svg>
         </section >
     )
 }
